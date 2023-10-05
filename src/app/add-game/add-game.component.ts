5bc1b95a9f6848525285fc93063a51dc5932d184
DavalEnzo <davalenzo@gmail.com>
2023-10-05 15:42:13 +0200

    ajout de toastr

--- src/app/add-game/add-game.component.ts
+++ src/app/add-game/add-game.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { GameService } from '../game.service';
+import { Router } from '@angular/router';
 import { Game } from '../../game.model';
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-add-game',
@@ -18,14 +20,19 @@ export class AddGameComponent {
     annee_sortie: ''
   };
 
-  constructor(private gameService: GameService) {}
+  constructor(private gameService: GameService, private router: Router, private toastrService: ToastrService) {}
 
   createGame() {
     this.gameService.createGame(this.newGame).subscribe(
       data => {
         console.log('Game created!', data);
+        this.router.navigate(['/games']);
+        this.toastrService.success('Le jeu a bien été ajouté !', 'Succès !', {timeOut: 3000, progressBar: true});
       },
-      error => console.error(error)
+      error => {
+        console.log('Erreur', error);
+        this.toastrService.error('Le jeu n\'a pas pu être ajouté !', 'Erreur !', {timeOut: 3000, progressBar: true});
+      }
     );
   }
 }
