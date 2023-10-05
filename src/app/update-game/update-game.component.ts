f9ca6f6b19f7ce43faa0537941f0ff8d96a0950c
Enzo Daval <81628324+DavalEnzo@users.noreply.github.com>
2023-10-05 11:52:16 +0200

    Crud api (#3)
    Co-authored-by: Moranoo <12mourad4@gmail.com>

--- src/app/update-game/update-game.component.ts
+++ src/app/update-game/update-game.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-update-game',
@@ -6,5 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-game.component.css']
 })
 export class UpdateGameComponent {
+  updateSuccess = false;
+  games: any[] = [];
+  selectedGameId: number;
+  updateGameData = {
+    popularite: null,
+    note_utilisateur: '',
+    titre: '',
+    genre: '',
+    nombre_vote: null,
+    histoire: '',
+    annee_sortie: ''
+  };
 
+  onGameChange() {
+    const selectedGame = this.games.find(game => game.id === this.selectedGameId);
+    if (selectedGame) {
+      this.updateGameData = { ...selectedGame };
+    } else {
+      this.updateGameData = {
+        popularite: null,
+        note_utilisateur: '',
+        titre: '',
+        genre: '',
+        nombre_vote: null,
+        histoire: '',
+        annee_sortie: ''
+      };
+    }
+  }
+  
+
+  constructor(private gameService: GameService) {
+    this.selectedGameId = 0;
+  }
+
+  ngOnInit() {
+    this.loadGames();
+  }
+
+  loadGames() {
+    this.gameService.getGames().subscribe(
+      data => this.games = data,
+      error => console.error(error)
+    );
+  }
+
+  updateGame() {
+    this.gameService.updateGame(this.selectedGameId, this.updateGameData).subscribe(  // Et ici
+      data => {
+        console.log('Game updated!', data);
+        this.loadGames();  // recharger la liste des jeux après la mise à jour
+      },
+      error => console.error(error)
+    );
+  }
 }
---
+++ src/game.model.ts
@@ -0,0 +1,11 @@
+export interface Game {
+    id?: number;
+    popularite: number;
+    note_utilisateur: string;
+    titre: string;
+    genre: string;
+    nombre_vote: number;
+    histoire: string;
+    annee_sortie: string;
+  }
+  
